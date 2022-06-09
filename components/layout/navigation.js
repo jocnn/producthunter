f02e2be455873cb621edfe39753932dd629a3370
jocnn <ijann_@outlook.com>
2022-06-09 11:29:53 -0300

    se creo el componente navigation

---
+++ components/layout/navigation.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const Navigation = () => {
+	return (
+		<nav>
+			<Link href={"/"}>Inicio</Link>
+			<Link href={"/"}>Populares</Link>
+			<Link href={"/"}>Nuevo Producto</Link>
+		</nav>
+	)
+}
+
+export default Navigation
