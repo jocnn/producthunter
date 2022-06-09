31748b18d3cb2da61070a789b27acece2b960ba6
jocnn <ijann_@outlook.com>
2022-06-09 11:26:30 -0300

    se creo la página about

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout/layout"
+
+const About = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Nosotros</h1>
+      </Layout>
+    </div>
+  )
+}
+
+export default About
