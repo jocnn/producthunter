897f6005880b69e9a61f4e31816e02c066509236
jocnn <ijann_@outlook.com>
2022-06-09 20:29:32 -0300

    se agrego estilos al header

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,14 +1,41 @@
 import Link from "next/link"
-import Search from "../ui/search"
+import styled from "@emotion/styled"
 
+import Search from "../ui/search"
 import Navigation from "./navigation"
+
+const Header_Styled = styled.header`
+  border-bottom: 2px solid var(--gris3);
+  padding: 1rem 0;
+`
+
+const Div_Styled = styled.div`
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media(min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`
+
+const P_Styled = styled.p`
+  color: var(--naranja);
+  font-size: 4rem;
+  line-height: 0;
+  font-weight: 700;
+  font-family: 'Roboto Slab', serif;
+  margin-right: 2rem;
+`
 
 const Header = () => {
 	return (
-		<header>
-			<div>
+		<Header_Styled>
+			<Div_Styled>
 				<div>
-					<p>P</p>
+          <Link href={'/'}>
+					  <P_Styled>P</P_Styled>
+          </Link>
 
 					<Search />
 
@@ -16,7 +43,7 @@ const Header = () => {
 
 				</div>
 				<div>
-        
+
           <p>Hola: usuario</p>
 
           <button
@@ -26,8 +53,8 @@ const Header = () => {
           <Link href={'/'}>Iniciar Sesión</Link>
           <Link href={'/'}>Crear Cuenta</Link>
         </div>
-			</div>
-		</header>
+			</Div_Styled>
+		</Header_Styled>
 	)
 }
 
