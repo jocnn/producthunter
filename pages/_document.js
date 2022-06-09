e8d4a70314f5d973ef75930552a19caa5a0395b6
jocnn <ijann_@outlook.com>
2022-06-09 20:27:14 -0300

    plantilla _document

--- pages/_document.js
+++ pages/_document.js
@@ -2,18 +2,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html  lang="es">
+    <Html lang="es">
 			<Head>
-				<title>Product Hunt Firebase y Next.JS</title>
 				<link
 					rel="stylesheet"
 					href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
 					integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
-					crossorigin="anonymous"
-					referrerpolicy="no-referrer"
+					crossOrigin="true"
+          content="strict-origin-when-cross-origin"
 				/>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" content="strict-origin-when-cross-origin" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="/static/css/app.css" />
 			</Head>
