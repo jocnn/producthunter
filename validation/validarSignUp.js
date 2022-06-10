154bea70f860225ebaa3fac4bed782dc9422c169
jocnn <ijann_@outlook.com>
2022-06-10 12:37:38 -0300

    se creo validadores de formulario

---
+++ validation/validarSignUp.js
@@ -0,0 +1,24 @@
+export default function validateSignUp(valores) {
+	let errores = {};
+
+	if (!valores.nombre) {
+		errores.nombre = "El nombre es obligatorio";
+	}
+
+	if (!valores.email) {
+		errores.email = "El correo electrónico es obligatorio";
+	} else if (
+		!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(valores.email)
+	) {
+		errores.mail = "Email no válido";
+	}
+
+	// validar el password
+	if (!valores.password) {
+		errores.password = "La contraseña es obligatorio";
+	} else if (valores.password.length < 6) {
+		errores.password = "La contraseña debe ser de al menos 6 caracteres";
+	}
+
+	return errores;
+}
