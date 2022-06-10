4e2d7b110aef0fd26840b1ccd80da8e67fd310eb
jocnn <ijann_@outlook.com>
2022-06-10 11:05:46 -0300

    se creo custom hook de validación

---
+++ hooks/useValidation.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+
+const useValidation = (stateInit, validar, fn) => {
+  
+  const [ valores, setValores ] = useState(stateInit)
+  const [ errores, setErrores ] = useState({})
+  const [ submitForm, setSubmitForm ] = useState(false)
+  
+  useEffect(() => {
+    if (submitForm) {
+      const noErrores = Object.keys(errores).length === 0
+
+      if (noErrores) {
+        fn()
+      }
+
+      setSubmitForm(false)
+    }
+  }, [])
+
+  const handleChange = e => {
+    setValores({
+      ...valores,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const erroresValidation = validar(valores)
+    setErrores(erroresValidation)
+    submitForm(true)
+  }
+
+  return {
+    valores,
+    errores,
+    submitForm,
+    handleChange,
+    handleSubmit,
+  };
+}
+
+export default useValidation
