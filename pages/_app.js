2a723808210c747c0d9ed1186e6c593fb5880017
jocnn <ijann_@outlook.com>
2022-06-11 15:55:27 -0300

    firebase - se agrego al arbol de componentes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { FirebaseProvider } from '../firebase/context/FirebaseProvider'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FirebaseProvider>
+      <Component {...pageProps} />
+    </FirebaseProvider>
+  )
 }
 
 export default MyApp
